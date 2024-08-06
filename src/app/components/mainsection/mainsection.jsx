75f307d9bf8f2b0e1bc1514f17fdd00237346f32
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2024-08-06 19:10:08 +0300

    Main

--- src/app/components/mainsection/mainsection.jsx
+++ src/app/components/mainsection/mainsection.jsx
@@ -12,24 +12,23 @@ export default function MainSection() {
                     <h1 className={styles.mainSectionH1}>Samet Can Gözüaçık</h1>
                 </div>
                 <p className={styles.mainSectionP}>Yazılım Geliştirici</p>
-                <p className={styles.mainSectionPTwo}>Merhaba, ben Ankara'da Freelance Web Developer olarak hizmet veriyorum. Uzmanlık alanlarım başlıca Web Tasarım ve Web Yazılım'dır. HTML5, CSS3, ve JavaScript teknolojileri yanı sıra, modern framework'ler olan Vue.js, React.js, Next.js, ve Quasar'ı kullanarak front-end geliştirme yapmaktayım. Ayrıca, PHP ve MySQL teknolojileri ile back-end programlama konusunda da deneyimliyim.
-
-Projelerinizde, kullanıcı dostu, mobil uyumlu ve performansı yüksek web siteleri tasarlayarak markanızın dijital dünyadaki varlığını güçlendirmeyi hedefliyorum.</p>
-                    <div>
+                <p className={styles.mainSectionPTwo}>Merhaba, ben Ankara&apos;da Freelance Web Developer olarak hizmet veriyorum. Uzmanlık alanlarım başlıca Web Tasarım ve Web Yazılım&apos;dır. HTML5, CSS3, ve JavaScript teknolojileri yanı sıra, modern framework&apos;ler olan Vue.js, React.js, Next.js, ve Quasar&apos;ı kullanarak front-end geliştirme yapmaktayım. Ayrıca, PHP ve MySQL teknolojileri ile back-end programlama konusunda da deneyimliyim.
+                    Projelerinizde, kullanıcı dostu, mobil uyumlu ve performansı yüksek web siteleri tasarlayarak markanızın dijital dünyadaki varlığını güçlendirmeyi hedefliyorum.</p>
+                <div>
                     <a
                         className={styles.mainSectionCV}
                         href="/mainsection/Samet_Can_Gözüaçık_CV.pdf"
                         download="Samet_Can_Gözüaçık_CV.pdf">
                         Özgeçmişimi İndir
                     </a>
-                </div> 
+                </div>
             </div>
             <Image
                 className={styles.mainSectionImage}
                 src={sametcan}
                 width={400}
                 height={500}
-                alt="Samet Can Gözüaçık"
+                alt="Samet_Can_Gözüaçık"
             />
         </section>
     );
